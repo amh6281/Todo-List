dc80367c3c7d9e70920c87345bb5be0af02b73ce
amh6281 <amh6281@naver.com>
2023-08-02 14:41:50 +0900

    Todos data fetch 2023.08.02

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import TodoForm from "./components/todoForm/TodoForm";
 import TodoList from "./components/todoList/TodoList";
 import "./global.scss";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8080/api/v1/todos?offset=0&limit=50"
+        );
+        setTodos(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchTodos();
+  }, []);
+
   return (
     <div className="container">
       <Header />
       <TodoForm />
-      <TodoList />
+      <TodoList todos={todos.value} />
     </div>
   );
 }
