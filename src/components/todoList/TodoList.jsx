4851e009c353a52acce5618f6d3e657a517a6de2
amh6281 <amh6281@naver.com>
2023-08-02 10:53:51 +0900

    TodoList design 2023.08.02

---
+++ src/components/todoList/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./todoList.scss";
+
+const TodoList = () => {
+  return (
+    <div className="todoList">
+      <span>등록된 [할 일]이 없습니다.</span>
+    </div>
+  );
+};
+
+export default TodoList;
