c13c72b6db07eedcf99db02120a9b4b90bc03041
amh6281 <amh6281@naver.com>
2023-08-02 10:49:13 +0900

    Global style 적용 2023.08.02

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/header/Header";
 import TodoForm from "./components/todoForm/TodoForm";
+import TodoList from "./components/todoList/TodoList";
+import "./global.scss";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <TodoForm />
-    </>
+      <TodoList />
+    </div>
   );
 }
 
